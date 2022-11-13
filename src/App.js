3e8aa585e4bb9e464567937dd02300c9fbc26797
AfonsoBatista07 <afonsobatista13@gmail.com>
2022-11-13 16:01:11 +0000

    Layouts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
+import Layout from "./layout/layout";
 
 function App() {
 
     return (
         <div>
+            <Layout>
+
+            </Layout>
         </div>
     );
 }
