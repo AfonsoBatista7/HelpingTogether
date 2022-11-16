585aa1fb94fcb7976fbda8448b562a39eb15477f
MarciaCampanha <m.campanha@campus.fct.unl.pt>
2022-11-16 13:19:05 +0000

    perfil

--- src/components/SectionsProfile/InfoProfile.js
+++ src/components/SectionsProfile/InfoProfile.js
@@ -2,42 +2,35 @@ import { Title } from "@mui/icons-material";
 import style from "./Profiles.module.css"
 import image from "../../images/people/Maria.jpg";
 
-import { Card, Grid, Typography, Container, Avatar, CardContent, Paper } from "@mui/material";
+import { Grid, Typography, Container, Avatar , Stack} from "@mui/material";
+import { width } from "@mui/system";
+import { blue } from "@mui/material/colors";
 
 function InfoProfile(props) {
   return (
-    <div>
-      <Grid
-        container
-        direction="column"
-        alignItems="left"
-        justifyContent="center"
-      >
-        <Paper containerStyle={{elevation:0}} >
-          
-            <Avatar
-              src={image}
-              sx={{ width: 200, height: 200 }}
-            />
-            <Typography style={{
-              fontWeight: 700,
-              fontSize: 30,
-              color: '#2E3B55',
-              textTransform: "uppercase",
-              textAlign: 'left'
-            }}>Maria</Typography>
+    <div style={{alignItems:'left'}}>
+     
+          <Avatar
+            src={image}
+            style={{ alignSelf: 'right' }}
+            sx={{ width: 200, height: 200 }}
+          />
+          <Typography style={{
+            fontWeight: 700,
+            fontSize: 30,
+            color: '#2E3B55',
+            textTransform: "uppercase",
+            textAlign: 'left'
+          }}>Maria</Typography>
 
-            <Typography style={{
-              fontWeight: 700,
-              fontSize: 30,
-              color: '#2E3B55',
-              textTransform: "uppercase",
-              textAlign: 'left'
-            }}>Leal</Typography>
-
-          
-        </Paper>
-      </Grid>
+          <Typography style={{
+            fontWeight: 700,
+            fontSize: 30,
+            color: '#2E3B55',
+            textTransform: "uppercase",
+            textAlign: 'left'
+          }}>Leal</Typography>
+     
 
     </div>
   );
