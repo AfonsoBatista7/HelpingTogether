4374109e05c5e3569c0b05ac0f5f8356b0b22ad0
Jose <jn.cunha@campus.fct.unl.pt>
2022-11-16 21:02:09 +0000

    Home page started
    feat. more cleaning

---
+++ src/layout/layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Home from "../components/home/Home";
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            <Header />
+              {children}
+            <Home />
+            <Footer />
+        </div>
+    );
+}
