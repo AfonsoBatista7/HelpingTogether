191c88ec679d12916f4faedd1a5940862213e2f3
MarciaCampanha <m.campanha@campus.fct.unl.pt>
2022-11-16 01:11:24 +0000

    estrutura voluntario e organizacao

--- src/pages/Organizacoes.js
+++ src/pages/Organizacoes.js
@@ -1,5 +1,43 @@
-function Organizacoes(){
-    return <div>Organizações</div>
+import style from "../components/SectionsProfile/Profiles.module.css"
+import { Pagination, Grid, Typography, Container } from "@mui/material";
+
+function Organizacoes() {
+    return (
+        <div className={style.backgroundwhite}>
+            <div>
+                <div className={style.margins}>
+                    <Container style={{
+                        height: 80
+                    }}></Container>
+                    <Typography
+                        style={{
+                            fontWeight: 700,
+                            fontSize: 30,
+                            color: '#2E3B55',
+                            textTransform: "uppercase",
+                            textAlign: 'left'
+                        }}
+                    >Organizações</Typography>
+
+                    <Container style={{
+                        height: 700
+                    }}></Container>
+                    <Grid
+                        container
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Pagination count={10} />
+                    </Grid>
+                    <Container style={{
+                        height: 50
+                    }}></Container>
+
+                </div >
+            </div>
+        </div>
+    );
 }
 
 export default Organizacoes;
