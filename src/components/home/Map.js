10c28b4ca0b20bfe32d99540ba01375b3199335b
Jose <jn.cunha@campus.fct.unl.pt>
2022-11-20 17:12:28 +0000

    I am losing my mind
    Carousel works
    Styling doesn't
    I am pain

--- src/components/home/Map.js
+++ src/components/home/Map.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import ptJson from "../../images/db3_Portugal.json";
+import { ComposableMap, Geographies, Geography, Annotation, ZoomableGroup } from "react-simple-maps";
+import ptJson from "./db3_Portugal.json";
 
 export default function Map() {
+
+  const geoURL = "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/portugal/portugal-districts.json";
+
   return (
-    <ComposableMap>
+    <ComposableMap width={1200} projectionConfig={{scale: 6000, center:[ -8.263495246008802, 39.774265042297884 ]}}>
       <Geographies geography={ptJson}>
         {({ geographies }) =>
           geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
+            <Geography key={geo.rsmKey} geography={geo} 
+              fill="#5FD"
+              fillOpacity="0.5"
+              stroke="#FFF"
+              strokeWidth={1}/>
           ))
         }
       </Geographies>
--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -11,7 +11,7 @@ import style from "./search.module.css";
 const SearchBar = () => {
   return (
     <div>
-        <Stack direction="row" spacing={0.5} sx={{marginLeft:15, marginTop:5}}>
+        <Stack direction="row" spacing={0.5}>
           <TextField id="" label="Procurar oportunidades" variant="outlined" size="small" className={style.searchBar}
             InputProps={{
               endAdornment: (
