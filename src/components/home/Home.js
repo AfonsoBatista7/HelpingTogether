39eb680ee62a081be8c7d61733491d41dc47a367
Jose <jn.cunha@campus.fct.unl.pt>
2022-11-21 19:23:11 +0000

    Fixing things

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,19 +12,19 @@ const Home = () => {
           <Grid container display='flex' align='center' alignItems='center' rowSpacing={20} style={{marginTop: 20}}>
 {/*             <Stack direction='column' alignItems='center' justifyContent='center' spacing={20}>
               <Stack direction="row" height={400} spacing={5}> */}
-              <Grid xs={2}>
+              <Grid item xs={2}>
                 
               </Grid>
-              <Grid xs={4}>
+              <Grid item xs={4}>
                 <SearchBar />
               </Grid>
-              <Grid xs={4}>
+              <Grid item xs={4}>
                 <Map />
               </Grid>
-              <Grid xs={2}>
+              <Grid item xs={2}>
                 
               </Grid>
-              <Grid xs={12}>
+              <Grid item xs={12}>
                 <HomeCarousel />
               </Grid>
           </Grid>
