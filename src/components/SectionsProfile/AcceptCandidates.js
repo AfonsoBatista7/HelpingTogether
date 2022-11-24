abee2633accdc36afbe1db7accc94b0f6c2db732
MarciaCampanha <m.campanha@campus.fct.unl.pt>
2022-11-24 17:44:50 +0000

    candidaturas funcionam

--- src/components/SectionsProfile/AcceptCandidates.js
+++ src/components/SectionsProfile/AcceptCandidates.js
@@ -1,13 +1,17 @@
 import style from "./Profiles.module.css"
 import { Pagination, Grid, Typography, Container, Divider, IconButton } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useReducer } from "react";
 import MiniBoxCandidate from "../StatsShowers/Box/MiniBoxCandidate";
 import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
 import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
 
 function AcceptCandidates(props) {
 
+    const [state, forceUpdate] = useReducer(x => x + 1, 0);
+
     const [peopleCandidate, setPeopleCandidate] = useState([])
+
+    const [peopleAccepted, setPeopleAccepted] = useState([])
 
     useEffect(() => {
 
@@ -21,7 +25,55 @@ function AcceptCandidates(props) {
 
         getVolunts(peopleCandidate)
 
-    }, [])
+    }, [state])
+
+    useEffect(() => {
+
+        const getAccepted = async () => {
+
+            const listAccepted = await fetchAccepted()
+
+            setPeopleAccepted(listAccepted)
+
+        }
+
+        getAccepted(peopleAccepted)
+
+    }, [state])
+
+    const fetchAccepted = async () => {
+        var res = await fetch(`http://localhost:5000/candidaturasAceites/`);
+
+        var data = await res.json()
+
+        var cand = []
+
+        for (const element of data) {
+            if (element.idVolunt === props.id) {
+                cand.push(element)
+            }
+        }
+
+        var list = []
+
+        for (const element of cand) {
+            var res1 = await fetch(`http://localhost:5000/login/${element.idPerson}`);
+            var res2 = await fetch(`http://localhost:5000/voluntarios/${element.idPerson}`);
+
+            var data1 = await res1.json()
+            var data2 = await res2.json()
+
+            if (Object.keys(data1).length !== 0) {
+                list.push(data1)
+            }
+            if (Object.keys(data2).length !== 0) {
+                list.push(data2)
+            }
+        }
+
+        return list;
+
+    }
 
     const fetchCandidatura = async () => {
         var res = await fetch(`http://localhost:5000/candidaturas/`);
@@ -59,21 +111,44 @@ function AcceptCandidates(props) {
     }
 
 
-    const acceptPerson = async (personId)=>{
-        addCandidaturaAceite({idVolunt: props.id, idPerson: personId})
+    const acceptPerson = async (personId) => {
+        await addCandidaturaAceite({ idVolunt: props.id, idPerson: personId })
         const res = await fetch('http://localhost:5000/candidaturas')
         const data = await res.json()
 
-        for(const element of data){
-            console.log(element)
-            if(element.idVolunt===props.id && element.idPerson===personId){
-                deleteCandidatura(element.id);
-                window.location.reload();
+        for (const element of data) {
+            if (element.idVolunt === props.id && element.idPerson === personId) {
+                await deleteCandidatura(element.id);
             }
         }
-
+        forceUpdate();
     }
-    
+
+    const rejectPerson = async (personId) => {
+        const res = await fetch('http://localhost:5000/candidaturas')
+        const data = await res.json()
+
+        for (const element of data) {
+            if (element.idVolunt === props.id && element.idPerson === personId) {
+               await deleteCandidatura(element.id);
+            }
+        }
+        forceUpdate();
+    }
+
+    const cancelPerson = async (personId) => {
+       await addCandidatura({ idVolunt: props.id, idPerson: personId })
+        const res = await fetch('http://localhost:5000/candidaturasAceites')
+        const data = await res.json()
+
+        for (const element of data) {
+            if (element.idVolunt === props.id && element.idPerson === personId) {
+                await deleteCandidaturaAceite(element.id);
+            }
+        }
+        forceUpdate();
+    }
+
 
     const addCandidaturaAceite = async (value) => {
         const res = await fetch('http://localhost:5000/candidaturasAceites', {
@@ -87,6 +162,7 @@ function AcceptCandidates(props) {
         const data = await res.json()
 
     }
+
 
     const addCandidatura = async (value) => {
         const res = await fetch('http://localhost:5000/candidaturas', {
@@ -133,12 +209,14 @@ function AcceptCandidates(props) {
             <Divider className={style.commentsProfile} />
             <Container >
                 <Grid container direction="row"
-                    justifyContent="center"
-                    alignItems="flex-start">
-                    <Grid item xs={6}
-                        justifyContent="center"
+                    justifyContent="space-between"
+                    alignItems="left">
+
+                    <Grid item xs={5} justifyContent="left"
+                        alignItems="left">
+                        <Grid container direction="column" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 16 }} justifyContent="left"
                         alignItems="center">
-                        <Grid container direction="column" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 16 }}>
+                             <Typography style={{marginLeft:"10%"}}>Candidatos por aceitar </Typography>
                             {!(peopleCandidate.length === 0) ? peopleCandidate.map((person, index) => (
                                 <><Grid container direction="row" item xs={2} sm={4} md={4} key={index}>
                                     <MiniBoxCandidate
@@ -148,10 +226,10 @@ function AcceptCandidates(props) {
                                         desc={person.description}
                                         rating={person.rating}
                                     ></MiniBoxCandidate>
-                                    <IconButton aria-label="delete" size="large" style={{ color: "green" }} onClick={function(){acceptPerson(person.id)}}>
+                                    <IconButton aria-label="delete" size="large" style={{ color: "green" }} onClick={function () { acceptPerson(person.id) }}>
                                         <CheckCircleOutlineRoundedIcon fontSize="inherit" />
                                     </IconButton>
-                                    <IconButton aria-label="delete" size="large" style={{ color: "red" }}>
+                                    <IconButton aria-label="delete" size="large" style={{ color: "red" }} onClick={function () { rejectPerson(person.id) }}>
                                         <HighlightOffRoundedIcon fontSize="inherit" />
                                     </IconButton>
                                 </Grid></>
@@ -169,15 +247,47 @@ function AcceptCandidates(props) {
                         </Grid>
                     </Grid>
 
-
-                    <Grid item xs={6}
+                    
+                       <Divider orientation="vertical" variant="middle" flexItem />
+               
+                    <Grid item xs={5}
                         justifyContent="center"
                         alignItems="center">
+
+                        <Grid container direction="column" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 16 }} justifyContent="center"
+                        alignItems="center" >
+                            <Typography  style={{marginLeft:"10%"}}>Candidatos Aceites </Typography>
+                            {!(peopleAccepted.length === 0) ? peopleAccepted.map((person, index) => (
+                                <><Grid container direction="row" item xs={2} sm={4} md={4} key={index}>
+                                    <MiniBoxCandidate
+                                        id={person.id}
+                                        image={person.image}
+                                        name={person.name}
+                                        desc={person.description}
+                                        rating={person.rating}
+                                    ></MiniBoxCandidate>
+                                    <IconButton aria-label="delete" size="large" style={{ color: "red" }} onClick={function () { cancelPerson(person.id) }}>
+                                        <HighlightOffRoundedIcon fontSize="inherit" />
+                                    </IconButton>
+                                </Grid></>
+                            )) : <><div className={style.voluntariadosProfile} style={{ marginTop: "5%", width: "100%" }}>
+                                <Typography style={{
+                                    fontWeight: 500,
+                                    fontSize: 20,
+                                    textAlign: 'center',
+                                    color: "grey",
+                                    marginLeft: 50
+                                }}>
+                                    Nenhum candidato aceite
+                                </Typography>
+                            </div></>}
+                        </Grid>
                     </Grid>
 
                 </Grid>
+
             </Container>
-        </div>
+        </div >
 
     );
 
