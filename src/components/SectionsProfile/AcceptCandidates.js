4947112b35d5183cbe4b87933fb5ab96f02a0a40
MarciaCampanha <m.campanha@campus.fct.unl.pt>
2022-11-24 14:40:15 +0000

    aceitar bem

--- src/components/SectionsProfile/AcceptCandidates.js
+++ src/components/SectionsProfile/AcceptCandidates.js
@@ -1,7 +1,9 @@
 import style from "./Profiles.module.css"
-import { Pagination, Grid, Typography, Container, Divider } from "@mui/material";
+import { Pagination, Grid, Typography, Container, Divider, IconButton } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import MiniBoxCandidate from "../StatsShowers/Box/MiniBoxCandidate";
+import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
 
 function AcceptCandidates(props) {
 
@@ -22,8 +24,6 @@ function AcceptCandidates(props) {
     }, [])
 
     const fetchCandidatura = async () => {
-        console.log(props.id)
-
         var res = await fetch(`http://localhost:5000/candidaturas/`);
 
         var data = await res.json()
@@ -53,8 +53,66 @@ function AcceptCandidates(props) {
             }
         }
 
+        console.log(list)
+
         return list;
     }
+
+
+    const acceptPerson = async (personId)=>{
+        addCandidaturaAceite({idVolunt: props.id, idPerson: personId})
+        const res = await fetch('http://localhost:5000/candidaturas')
+        const data = await res.json()
+
+        for(const element of data){
+            console.log(element)
+            if(element.idVolunt===props.id && element.idPerson===personId){
+                deleteCandidatura(element.id);
+                window.location.reload();
+            }
+        }
+
+    }
+    
+
+    const addCandidaturaAceite = async (value) => {
+        const res = await fetch('http://localhost:5000/candidaturasAceites', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(value),
+        })
+
+        const data = await res.json()
+
+    }
+
+    const addCandidatura = async (value) => {
+        const res = await fetch('http://localhost:5000/candidaturas', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(value),
+        })
+
+        const data = await res.json()
+
+    }
+
+    const deleteCandidaturaAceite = async (id) => {
+        const res = await fetch(`http://localhost:5000/candidaturasAceites/${id}`, {
+            method: 'DELETE',
+        })
+    }
+
+    const deleteCandidatura = async (id) => {
+        const res = await fetch(`http://localhost:5000/candidaturas/${id}`, {
+            method: 'DELETE',
+        })
+    }
+
 
     return (
         <div >
@@ -74,38 +132,51 @@ function AcceptCandidates(props) {
             >Candidatos Pendentes</Typography>
             <Divider className={style.commentsProfile} />
             <Container >
-                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 16 }}>
-                        {!(peopleCandidate.length === 0) ? peopleCandidate.map((person, index) => (
-                            <><Grid item xs={2} sm={4} md={4} key={index}>
-                                <MiniBoxCandidate
-                                    id={person.id}
-                                    image={person.image}
-                                    name={person.name}
-                                    desc={person.description}
-                                    rating={person.rating}
-                                ></MiniBoxCandidate>
-                            </Grid></>
-                        )) : <><div className={style.voluntariadosProfile} style={{ marginTop: "5%", width: "100%" }}>
-                            <Typography style={{
-                                fontWeight: 500,
-                                fontSize: 20,
-                                textAlign: 'center',
-                                color: "grey",
-                                marginLeft: 50
-                            }}>
-                                Nenhum candidato
-                            </Typography>
-                        </div></>}
-                    </Grid></Container>
-                {!(peopleCandidate.length === 0) ?
-                    <Grid
-                        container
-                        direction="row"
-                        alignItems="center"
+                <Grid container direction="row"
+                    justifyContent="center"
+                    alignItems="flex-start">
+                    <Grid item xs={6}
                         justifyContent="center"
-                    >
-                        <Pagination count={1} className={style.pagination} />
-                    </Grid> : <></>}
+                        alignItems="center">
+                        <Grid container direction="column" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 16 }}>
+                            {!(peopleCandidate.length === 0) ? peopleCandidate.map((person, index) => (
+                                <><Grid container direction="row" item xs={2} sm={4} md={4} key={index}>
+                                    <MiniBoxCandidate
+                                        id={person.id}
+                                        image={person.image}
+                                        name={person.name}
+                                        desc={person.description}
+                                        rating={person.rating}
+                                    ></MiniBoxCandidate>
+                                    <IconButton aria-label="delete" size="large" style={{ color: "green" }} onClick={function(){acceptPerson(person.id)}}>
+                                        <CheckCircleOutlineRoundedIcon fontSize="inherit" />
+                                    </IconButton>
+                                    <IconButton aria-label="delete" size="large" style={{ color: "red" }}>
+                                        <HighlightOffRoundedIcon fontSize="inherit" />
+                                    </IconButton>
+                                </Grid></>
+                            )) : <><div className={style.voluntariadosProfile} style={{ marginTop: "5%", width: "100%" }}>
+                                <Typography style={{
+                                    fontWeight: 500,
+                                    fontSize: 20,
+                                    textAlign: 'center',
+                                    color: "grey",
+                                    marginLeft: 50
+                                }}>
+                                    Nenhum candidato
+                                </Typography>
+                            </div></>}
+                        </Grid>
+                    </Grid>
+
+
+                    <Grid item xs={6}
+                        justifyContent="center"
+                        alignItems="center">
+                    </Grid>
+
+                </Grid>
+            </Container>
         </div>
 
     );
