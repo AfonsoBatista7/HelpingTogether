cfae2cb6ac8552bbce155ee347b3be8d2c8e005d
MarciaCampanha <m.campanha@campus.fct.unl.pt>
2022-11-28 14:49:54 +0000

    break word

--- src/components/SectionsProfile/InfoVoluntariado.js
+++ src/components/SectionsProfile/InfoVoluntariado.js
@@ -213,7 +213,7 @@ function InfoVoluntariado(props) {
                                 justifyContent="space-between"
                                 alignItems="center">
 
-
+                                <Grid item>
                                 <div className={style.descriptionvoluntario} >
                                     <Typography style={{
                                         fontWeight: 700,
@@ -224,10 +224,12 @@ function InfoVoluntariado(props) {
                                         textAlign: 'left'
                                     }}>{props.name}</Typography>
                                 </div>
-
+                                </Grid>
+                                <Grid item>
                                 <div className={style.rating}>
                                     <Rating name="half-rating-read" defaultValue={props.rating} precision={1} readOnly size="large" />
                                 </div>
+                                </Grid>
                             </Grid>
 
                         </div>
